de70607495a7dd2531cecfeaed9ee1308e6c23c7
kyberspatz <105124054+kyberspatz@users.noreply.github.com>
2022-08-23 22:51:13 +0200

    corrected setup rewritings

--- setup.php
+++ setup.php
@@ -39,7 +39,7 @@ if(!is_file("phpqrcode/qrimage_original.php"))
 	/*Additionally added code*/
             $px   = (imagesx($base_image) - 5 * strlen(QRUNIQID)) / 2;  
             $color = imagecolorallocate($base_image, 0, 0, 0); 
-            imagestring($base_image, 0, $px, 58, QRUNIQID, $color);
+            imagestring($base_image, 0, $px, 60, QRUNIQID, $color);
 	/*Additionally added code (end)*/
 			',$content);
 		
@@ -49,6 +49,30 @@ if(!is_file("phpqrcode/qrimage_original.php"))
 		
 		file_put_contents("phpqrcode/qrimage.php",$content);
 	}
+}
+
+else {
+	$file = "phpqrcode/qrimage.php";
+	if(is_file($file))
+	{
+		file_put_contents("phpqrcode/qrimage_original.php",file_get_contents("phpqrcode/qrimage.php"));
+		$content = file_get_contents("phpqrcode/qrimage.php");
+		$content = str_replace('imagefill($base_image, 0, 0, $col[0]);','imagefill($base_image, 0, 0, $col[0]);
+            
+	/*Additionally added code*/
+            $px   = (imagesx($base_image) - 5 * strlen(QRUNIQID)) / 2;  
+            $color = imagecolorallocate($base_image, 0, 0, 0); 
+            imagestring($base_image, 0, $px, 60, QRUNIQID, $color);
+	/*Additionally added code (end)*/
+			',$content);
+		
+		$content = str_replace('$imgW = $w + 2*$outerFrame;','$imgW = $w + 3*$outerFrame; //edited from 2 to 3',$content);
+		$content = str_replace(' $imgH = $h + 2*$outerFrame;',' $imgH = $h + 3*$outerFrame; //edited from 2 to 3',$content);
+		
+		
+		file_put_contents("phpqrcode/qrimage.php",$content);
+	}
+	
 }
 
 if(is_dir("phpqrcode") && is_dir("images") && is_dir("kartons"))
